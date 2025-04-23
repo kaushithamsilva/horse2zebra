31b9ef0e8cc58b05f7c26b2581058f1f4c654d4f
kaushithamsilva <kaushithamsilva@gmail.com>
2025-04-24 02:42:12 +0530

    fixed dataloader

--- code/scripts/load_data.py
+++ code/scripts/load_data.py
@@ -9,39 +9,40 @@ class DataLoader:
         self.data_path = data_path
         self.batch_size = batch_size
 
-    def load_images(self, folder_name):
-        """"Load images from the specified folder and normalize them."""
-        path_name = os.path.join(self.data_path, folder_name)
-        print(f"Loading images from {path_name}...")
-        if not os.path.exists(path_name):
-            raise FileNotFoundError(f"Data folder {path_name} does not exist.")
-        dataset = tf.keras.utils.image_dataset_from_directory(
-            path_name,
-            label_mode=None,  # No labels since this is unsupervised
-            image_size=(256, 256),  # Resize images to 256x256
-            batch_size=self.batch_size  # Batch size
-        )
-        return self._normalize_dataset(dataset)  # Normalize images to [0, 1]
-
-    def _normalize_dataset(self, dataset):
-        return dataset.map(lambda x: x / 255.0)
-
     def get_training_data(self, split='train'):
         """Load and combine normalized data for the specified split (train or test)."""
-        horse_dataset = self.load_images(f"{split}A")
-        zebra_dataset = self.load_images(f"{split}B")
 
-        # Create labels for the datasets: 0 for horse, 1 for zebra
-        horse_labels = tf.data.Dataset.from_tensor_slices(
-            tf.zeros(horse_dataset.cardinality(), dtype=tf.int32))
-        zebra_labels = tf.data.Dataset.from_tensor_slices(
-            tf.ones(zebra_dataset.cardinality(), dtype=tf.int32))
+        # Load images from directories
+        horse_dir = os.path.join(self.data_path, f"{split}A")
+        zebra_dir = os.path.join(self.data_path, f"{split}B")
 
-        # Combine datasets and labels
-        horse_data = tf.data.Dataset.zip((horse_dataset, horse_labels))
-        zebra_data = tf.data.Dataset.zip((zebra_dataset, zebra_labels))
+        # Count files to create appropriate label arrays
+        horse_files = len([f for f in os.listdir(horse_dir)
+                          if os.path.isfile(os.path.join(horse_dir, f))])
+        zebra_files = len([f for f in os.listdir(zebra_dir)
+                          if os.path.isfile(os.path.join(zebra_dir, f))])
 
-        # Concatenate horse and zebra datasets
-        combined_dataset = horse_data.concatenate(zebra_data)
+        # Create datasets with labels directly
+        horse_dataset = tf.keras.utils.image_dataset_from_directory(
+            horse_dir,
+            labels=tf.zeros(horse_files, dtype=tf.int32),
+            label_mode="int",
+            image_size=(256, 256),
+            batch_size=self.batch_size
+        )
 
+        zebra_dataset = tf.keras.utils.image_dataset_from_directory(
+            zebra_dir,
+            labels=tf.ones(zebra_files, dtype=tf.int32),
+            label_mode="int",
+            image_size=(256, 256),
+            batch_size=self.batch_size
+        )
+
+        # Normalize images
+        horse_dataset = horse_dataset.map(lambda x, y: (x / 255.0, y))
+        zebra_dataset = zebra_dataset.map(lambda x, y: (x / 255.0, y))
+
+        # Concatenate datasets
+        combined_dataset = horse_dataset.concatenate(zebra_dataset)
         return combined_dataset.shuffle(buffer_size=1000).prefetch(tf.data.AUTOTUNE)
